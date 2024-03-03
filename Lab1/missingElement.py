26316ea1b21776e7ee93ce41d7973b1f75b7eb1f
Adrian Suliga <adriansuliga81@gmail.com>
2024-03-03 20:21:56 +0100

    Solved exercises to find missing number in linear time (however logarithmic is still possible)

--- Lab1/missingElement.py
+++ Lab1/missingElement.py
@@ -8,3 +8,22 @@ m = 12
 """
 #Przechodzimy po elementach tablicy kolejno sprawdzając czy kolejny element ciągu jest równy indeksowi
 #logarytmicznie: zaczynamy od środka i sprawdzamy czy indeksy się zgadzają
+from random import randrange
+def searchArray(T):
+    for i in range(len(T)):
+        if T[i] != i:
+            return i
+    return T[-1] + 1
+def sortArray(T):
+    n = len(T)
+    for i in range(1, n):
+        toInsert = T[i]
+        j = i - 1
+        while j > -1 and toInsert < T[j]:
+            T[j+1] = T[j]
+            j -= 1
+        T[j+1] = toInsert
+T = [0, 1, 2, 3, 6, 7]
+sortArray(T)
+print(T)
+print(searchArray(T))
