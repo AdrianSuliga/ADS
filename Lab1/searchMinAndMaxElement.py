ec2aa724eaa68914d9f8fd7c849cb4016eab412d
Adrian Suliga <adriansuliga81@gmail.com>
2024-03-03 16:05:01 +0100

    solved exercises: find smallest and largest nums in array in O(3n/2) time and find out if there are numbers i,j such that T[j]-T[i]==target

--- Lab1/searchMinAndMaxElement.py
+++ Lab1/searchMinAndMaxElement.py
@@ -1,12 +1,23 @@
 """
 find smallest and largest number in the array
 """
+from random import randrange
 def lookForSmallestAndGreatest(T):
     mini, maks = float('inf'), -float('inf')
     for i in range(0, len(T), 2):
         if (T[i] < T[i+1]):
-            if T[i] < min: pass
-            if T[i+1] > max: pass
+            if T[i] < mini:
+                mini = T[i]
+            if T[i+1] > maks:
+                maks = T[i+1]
         else:
-            if T[i+1] < min: pass
-            if T[i] > max: pass
+            if T[i+1] < mini:
+                mini = T[i+1]
+            if T[i] > maks:
+                maks = T[i]
+    return (mini, maks)
+
+
+T = [randrange(10, 100) for _ in range(100)]
+print(T)
+print(lookForSmallestAndGreatest(T))
--- Lab1/searchSortedArray.py
+++ Lab1/searchSortedArray.py
@@ -1,4 +1,31 @@
 """
 Czy istnieją i,j w posortowanej rosnąco tablicy takie że T[j] - T[i] == x 
 """
-#Przesuwać wskaźniki od końców          
+#Przesuwać wskaźniki od końców (liniowe)
+from random import randrange
+def sortT(T):
+    n = len(T)
+    for i in range(1, n):
+        toInsert = T[i]
+        j = i - 1
+        while j > -1 and T[j] > toInsert:
+            T[j + 1] = T[j]
+            j -= 1
+        T[j + 1] = toInsert
+def searchT(T, x):
+    i, j = 0, len(T) - 1
+    while i < j:
+        if T[j] - T[i] == x:
+            return True
+        elif T[j-1] - T[i] == x:
+            return True
+        elif T[j] - T[i+1] == x:
+            return True
+        j-=1
+        i+=1
+    return False
+
+T = [randrange(1, 10) for _ in range(20)]
+sortT(T)
+print(searchT(T, 5))
+print(T)
