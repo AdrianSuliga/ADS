0e39f2621521c77e19416b42d48b96a5892633e4
Adrian Suliga <adriansuliga81@gmail.com>
2024-03-08 15:34:53 +0100

    Reverse linked list recursively

--- Lab1/inverseLinkedList.py
+++ Lab1/inverseLinkedList.py
@@ -12,7 +12,17 @@ class Node:
         print(None)
     def gen_list():
         e3 = Node(6, None)
-        return e3
+        e2 = Node(9, e3)
+        e1 = Node(1, e2)
+        e0 = Node(4, e1)
+        return e0
+    def reverse_rec(p):
+        if p == None or p.next == None:
+            return p
+        q = Node.reverse_list(p.next)
+        p.next.next = p
+        p.next = None
+        return q
     def reverse_list(p):
         if p == None or p.next == None:
             return p
@@ -26,8 +36,7 @@ class Node:
         p.next = q
         return p
 
-
 p = Node.gen_list()
 p.print_list()
-p = p.reverse_list()
+p = p.reverse_rec()
 p.print_list()
