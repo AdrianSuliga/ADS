5782603b202f22f1bb6e35ca6360d2a369e6d5fc
Adrian Suliga <adriansuliga81@gmail.com>
2024-03-09 16:24:52 +0100

    implemented merging two sorted linked lists

--- Lab2/mergeTwoSortedLinkedLists.py
+++ Lab2/mergeTwoSortedLinkedLists.py
@@ -1,13 +1,38 @@
 """
 p1, p2 - posortowane listy odsyłaczowe. scal je w 1 posortowaną listę
 """
-# iteracyjnie: jak w merge sorcie
 class Node:
     def __init__(self, val, next = None):
         self.val = val
         self.next = next
+
     def merge_lists_it(p, q):
-        pass
+        result = Node(None, None)
+        start = result
+
+        while p != None and q != None:
+            if p.val <= q.val:
+                result.next = p
+                p = p.next
+            else:
+                result.next = q
+                q = q.next
+            result = result.next
+
+        while p != None:
+            result.next = p
+            result = result.next
+            p = p.next
+        while q != None:
+            result.next = q
+            result = result.next
+            q = q.next
+        
+        buffer = start.next
+        start.next = None
+
+        return buffer
+    
     def merge_lists_rec(p, q):
         if p == None: return q
         if q == None: return p
@@ -16,6 +41,7 @@ class Node:
             return p
         q.next = Node.merge_lists_rec(p, q.next)
         return q
+    
     def gen_p():
         e3 = Node(7)
         e2 = Node(5, e3)
@@ -27,6 +53,7 @@ class Node:
         e1 = Node(4, e2)
         e0 = Node(1, e1)
         return e0
+    
     def print_list(p):
         while p is not None:
             print(p.val, "-> ", end='')
@@ -37,5 +64,5 @@ p = Node.gen_p()
 q = Node.gen_q()
 p.print_list()
 q.print_list()
-m = Node.merge_lists_rec(p, q)
+m = Node.merge_lists_it(p, q)
 m.print_list()
