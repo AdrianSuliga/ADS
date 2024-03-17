b07f28f79923662272eed7e26885b0f62969785a
Adrian Suliga <adriansuliga81@gmail.com>
2024-03-17 23:43:15 +0100

    Tried implementing n logp algorithm for online 2 but it didn't work

---
+++ Lab3/binarySearchForOffline2.py
@@ -0,0 +1,67 @@
+def heap_sort(T):
+    n = len(T)
+    build_heap(T)
+    for i in range(n - 1, 0, -1):
+        T[0], T[i] = T[i], T[0]
+        heapify(T, i, 0)
+
+def build_heap(T):
+    n = len(T)
+    for i in range(parent(n - 1), -1, -1):
+        heapify(T, n, i)
+
+def heapify(T, n, i):
+    l, r = left(i), right(i)
+    mi = i
+
+    if l < n and T[l] < T[mi]: mi = l
+    if r < n and T[r] < T[mi]: mi = r
+
+    if i != mi:
+        T[mi], T[i] = T[i], T[mi]
+        heapify(T, n, mi)
+
+def left(i):
+    return 2 * i + 1
+def right(i):
+    return 2 * i + 2
+def parent(i):
+    return (i - 1) // 2
+
+def ksum(T, k, p):
+    n = len(T)
+    sum = 0
+    A = T[:p]
+    heap_sort(A)
+    for i in range(n - p + 1):
+        sum += A[k-1]
+        if i + p < n:
+            binary_insert(A, T[i + p])
+        A.remove(T[i])
+    return sum
+
+def binary_insert(T:list, val):
+    def find(T, ind, val, left, right, result):
+        if left > right: return -1
+        if T[ind] <= val:
+            right = ind - 1
+        else:
+            result = ind
+            left = ind + 1
+        return find(T, (left + right) // 2, val, left, right, result)
+
+    if T[0] < val:
+        T.insert(0, val)
+        return
+    if T[-1] > val:
+        T.append(val)
+        return
+    
+    n = len(T)
+    res_ind = find(T, n // 2, val, 0, n - 1, n // 2)
+    T.insert(res_ind + 1, val)
+
+T =  [7, 9, 1, 5, 8, 6, 2, 12]
+k =  4
+p =  5
+print(ksum(T, k, p))
--- Offline/Ex2/zad2.py
+++ Offline/Ex2/zad2.py
@@ -1,4 +1,4 @@
-# Program na początku sortuje pierwszy przedział [0 ... p - 1]
+# Program na początku sortuje pierwszy przedział [0 ... p - 1] w O(p logp)
 # następnie wrzuca k największych liczb z tego przedziału to min_heapa
 # a resztę do max_heapa. W korzeniu min_heapa jest na początku k. największy
 # element z [0 ... p - 1]. Następnie sprawdzamy co się stanie gdy ruszymy okno o 1.
@@ -15,9 +15,14 @@
 # Jako że funkcje heapujące zamieniają kolejność elementów to w kopcach musimy przechowywać indeksy elementów w tablicy obok ich wartości,
 # żeby wiedzieć które są a które nie są w rozważanym aktualnie przedziale.
 
+# jako że kopiec min_heap może rozrosnąć się w najgorszym przypadku do n, a operacje na nim wykonywane są w czasie
+# logarytmicznym, to dostajemy log(n) operacji na kopce. pętla for() wykonuje się (n - p) razy, w najgorszym wypadku n, zatem
+# ostateczna złożoność algorytmu wynosi O(n*log(n))
+
 from zad2testy import runtests
+
 def ksum(T, k, p):
-    n = len(T)
+    n, sum = len(T), 0
 
     spare_array = [(T[i], i) for i in range(p)]
     merge_sort_modified(spare_array)
@@ -32,45 +37,44 @@ def ksum(T, k, p):
     build_heap(min_heap, True)
     build_heap(max_heap, False)
 
-    return calc_sum(T, max_heap, min_heap, is_in_min_heap, (n, p))
-
-def calc_sum(T:list, max_heap:list, min_heap:list, is_in_min_heap:list, bundle:tuple):
-    sum = 0
-    n, p = bundle
     for i in range(n - p + 1):
-        while len(min_heap) > 0:
-            kVal, kInd = min_heap[0]
-            if i <= kInd <= i + p - 1: 
-                break
-            else:
-                is_in_min_heap[kInd] = False
-                heap_pop(min_heap, True)
-        sum += kVal
+        sum += calc_kth(T, max_heap, min_heap, is_in_min_heap, n, i, p)
 
-        if is_in_min_heap[i] and i + p < n: # jeśli element tracony jest w min heapie i możemy przesunąć się dalej
-            heap_insert(max_heap, T[i + p], i + p, False)
-            while len(max_heap) > 0:
-                mVal, mInd = max_heap[0]
-                heap_pop(max_heap, False)
-                is_in_min_heap[mInd] = False
-                if i + 1 <= mInd <= i + p: break
-            heap_insert(min_heap, mVal, mInd, True)
-            is_in_min_heap[mInd] = True
-        elif not is_in_min_heap[i] and i + p < n:
-            heap_insert(max_heap, T[i + p], i + p, False)
-            while max_heap[0][0] > min_heap[0][0]:
-                if i + 1 <= max_heap[0][1] <= i + p:
-                    is_in_min_heap[min_heap[0][1]] = False
-                    is_in_min_heap[max_heap[0][1]] = True
-                    min_heap[0], max_heap[0] = max_heap[0], min_heap[0]
-                    heapify(min_heap, len(min_heap), 0, True)
-                    heapify(max_heap, len(max_heap), 0, False)
-                    break
-                else:
-                    heap_pop(max_heap, False)
     return sum
 
-def heap_insert(heap:list, val, ind, heap_flag):
+def calc_kth(T:list, max_heap:list, min_heap:list, is_in_min_heap:list, n:int, i:int, p:int):
+    while len(min_heap) > 0:
+        kVal, kInd = min_heap[0]
+        if i <= kInd <= i + p - 1: 
+            break
+        else:
+            is_in_min_heap[kInd] = False
+            heap_pop(min_heap, True)
+
+    if is_in_min_heap[i] and i + p < n:
+        heap_insert(max_heap, T[i + p], i + p, False)
+        while len(max_heap) > 0:
+            mVal, mInd = max_heap[0]
+            heap_pop(max_heap, False)
+            is_in_min_heap[mInd] = False
+            if i + 1 <= mInd <= i + p: break
+        heap_insert(min_heap, mVal, mInd, True)
+        is_in_min_heap[mInd] = True
+    elif not is_in_min_heap[i] and i + p < n:
+        heap_insert(max_heap, T[i + p], i + p, False)
+        while max_heap[0][0] > min_heap[0][0]:
+            if i + 1 <= max_heap[0][1] <= i + p:
+                is_in_min_heap[min_heap[0][1]] = False
+                is_in_min_heap[max_heap[0][1]] = True
+                min_heap[0], max_heap[0] = max_heap[0], min_heap[0]
+                heapify(min_heap, len(min_heap), 0, True)
+                heapify(max_heap, len(max_heap), 0, False)
+                break
+            else:
+                heap_pop(max_heap, False)
+    return kVal
+
+def heap_insert(heap:list, val:int, ind:int, heap_flag:bool):
     heap.append((val, ind))
     pos = len(heap) - 1
     if heap_flag:
@@ -82,39 +86,32 @@ def heap_insert(heap:list, val, ind, heap_flag):
             heap[parent(pos)], heap[pos] = heap[pos], heap[parent(pos)]
             pos = parent(pos)
 
-def heap_pop(heap:list, heap_flag): # remove first element and maintain heap structure (heapify broken element)
+def heap_pop(heap:list, heap_flag:bool):
     heap[0], heap[-1] = heap[-1], heap[0]
     heap.pop()
     heapify(heap, len(heap), 0, heap_flag)
 
-def build_heap(T, heap_flag): # if heap_flag: build min_heap
+def build_heap(T, heap_flag):
     n = len(T)
     for i in range(parent(n - 1), -1, -1):
         heapify(T, n, i, heap_flag)
 
-def heapify(T, n, i, heap_flag): # if heap_flag: build min_heap
+def heapify(T, n, i, heap_flag):
     l, r = left(i), right(i)
     mi = i
     if heap_flag:
-        if l < n and T[l][0] < T[mi][0]:
-            mi = l
-        if r < n and T[r][0] < T[mi][0]:
-            mi = r
+        if l < n and T[l][0] < T[mi][0]: mi = l
+        if r < n and T[r][0] < T[mi][0]: mi = r
     else:
-        if l < n and T[l][0] > T[mi][0]:
-            mi = l
-        if r < n and T[r][0] > T[mi][0]:
-            mi = r
+        if l < n and T[l][0] > T[mi][0]: mi = l
+        if r < n and T[r][0] > T[mi][0]: mi = r
     if mi != i:
         T[mi], T[i] = T[i], T[mi]
         heapify(T, n, mi, heap_flag)
 
-def left(i):
-    return 2 * i + 1
-def right(i):
-    return 2 * i + 2
-def parent(i):
-    return (i - 1) // 2
+def left(i): return 2 * i + 1
+def right(i): return 2 * i + 2
+def parent(i): return (i - 1) // 2
 
 def merge_sort_modified(T):
     n = len(T)
@@ -141,6 +138,7 @@ def merge_sort_modified(T):
         T[k] = R[j]
         j += 1
         k += 1
+
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( ksum, all_tests=True )
     
