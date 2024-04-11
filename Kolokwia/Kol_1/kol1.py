68cefeb8d12759800f0234612ca6a4413cf3c4ec
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-11 14:02:33 +0200

    Added exs from test 1 23_24 and git ignore file

---
+++ Kolokwia/Kol_1/kol1.py
@@ -0,0 +1,20 @@
+from kol1testy import runtests
+
+def maxrank(T):
+	n = len(T)
+
+	is_outranked = [False for _ in range(n)]
+
+	max_rank = -1
+	for i in range(n - 1, -1, -1):
+		if is_outranked[i]: continue
+		rank = 0
+		for j in range(i):
+			if T[j] < T[i]: 
+				is_outranked[j] = True
+				rank += 1
+		max_rank = max(max_rank, rank)
+		if max_rank > i: break
+	return max_rank
+# zmien all_tests na True zeby uruchomic wszystkie testy
+runtests( maxrank, all_tests = True )
