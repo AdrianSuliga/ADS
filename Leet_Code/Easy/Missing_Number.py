bb6033148396ed08398355b5c4c753d2c9d54b28
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-20 22:36:31 +0200

    missing number exs

---
+++ Leet_Code/Easy/Missing_Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        size = len(nums)
+        result = size
+        for i in range(size):
+            result += i - nums[i]
+        return result      
+
+T = [9,6,4,2,3,5,7,0,1]
+S = Solution()
+print(S.missingNumber(T))
+
+"""# brute force O(n logn)
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        for i in range(n):
+            if i != nums[i]: return i
+        return n"""
+        
