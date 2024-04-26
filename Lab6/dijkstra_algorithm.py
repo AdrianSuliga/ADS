d07e26986731c0e6322651b12ebccb2d24432cfc
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-26 13:17:46 +0200

    implement dijkstra algorithm

--- Lab6/dijkstra_algorithm.py
+++ Lab6/dijkstra_algorithm.py
@@ -1,9 +1,29 @@
-def shortest_path(G, u, v):
-    pass
+from queue import PriorityQueue
+def shortest_path(G:list, s:int, e:int) -> list:
+    n = len(G)
+    parent = [None for _ in range(n)]
+    d = [float('inf') for _ in range(n)]
+    Q = PriorityQueue() # Priority Queue, pierwsze pole to priorytet, drugie to wartość
 
+    for i in range(n): # defaultowo priority queue przechowuje na początku elementy z min priorytetem
+        Q.put((float('inf'), i))
+    d[s] = 0
 
-
-
+    while not Q.empty():
+        _, u = Q.get()
+        for v in G[u]:
+            if v[1] + d[u] < d[v[0]]:
+                d[v[0]] = v[1] + d[u]
+                parent[v[0]] = u
+                Q.put((d[v[0]], v[0]))
+    result = []
+    result.append(e)
+    buffor = parent[e]
+    while buffor != s:
+        result.append(buffor)
+        buffor = parent[buffor]
+    result.append(s)
+    return result
 
 G = [
     [(1, 2), (2, 4)], # a 0
@@ -13,3 +33,4 @@ G = [
     [(2, 1), (5, 3)], # e 4
     [(3, 1), (4, 3)]  # f 5
 ]
+print(shortest_path(G, 0, 5))
