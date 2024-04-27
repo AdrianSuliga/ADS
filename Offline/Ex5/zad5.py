69b3266f16920bcfa042c5529e79c8fa8af50cd1
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-27 16:44:48 +0200

    improvement of offline 5

--- Offline/Ex5/zad5.py
+++ Offline/Ex5/zad5.py
@@ -1,9 +1,15 @@
 from queue import PriorityQueue
 from zad5testy import runtests
 
+# Adrian Suliga
+# Algorytm dodaje do grafu jeden wierzchołek, połączony z każdą z osobliwości krawędzią o koszcie 0
+# robi to poprzez dodanie S krawędzi (wierzchołek, osobliwość, 0). Następnie graf jest przepisywany na 
+# reprezentację listową, na której uruchamiany jest algorytm Dijkstry. Złożoność czasową algorytmu szacuję
+# na O((E + V)logV), czyli O(ElogV), natomiast pamięciową na O(E + V) z powodu reprezentacji grafu i tablic pomocniczych
+
 def spacetravel( n, E, S, a, b ):
-    new_vertexes(E, S) # dodanie S^2/2 krawędzi, O(S^2)
-    G = list_form(E) # Przepisanie na reprezentację listową O(E + S^2)
+    new_vertex(E, S) # dodanie S krawędzi
+    G = list_form(E) # Przepisanie na reprezentację listową O(E + V)
     size = len(G)
     
     parent = [None for _ in range(size)]
@@ -14,8 +20,8 @@ def spacetravel( n, E, S, a, b ):
     for i in range(size):
         Q.put((distance[i], i))
     
-    while not Q.empty(): # Algorytm Dijkstry O((E + S^2)logV) gdzie S może być rzędu V, mamy więc O((E + V^2)logV) przy czym
-        _, u = Q.get()  # E < V^2, zatem dostajemy O(V^2logV)
+    while not Q.empty(): # Algorytm Dijkstry O((E + S)log(E + S)) gdzie S może być rzędu V, mamy więc O((E + V)logV)
+        _, u = Q.get()
         for v, cost in G[u]:
             if cost + distance[u] < distance[v]:
                 distance[v] = cost + distance[u]
@@ -24,15 +30,8 @@ def spacetravel( n, E, S, a, b ):
 
     if distance[b] == float('inf'): return None
     else: return distance[b]
-    
 
-def new_vertexes(E:list, S:list) -> None:
-    n = len(S)
-    for i in range(n):
-        for j in range(i + 1, n):
-            E.append((S[i], S[j], 0))
-
-def list_form(E:list) -> list: # O(E + S^2)
+def list_form(E:list) -> list: # O(E + S), czyli O(E + V)
     V = size_of_graph(E)
     G = [[] for _ in range(V)]
 
@@ -42,7 +41,12 @@ def list_form(E:list) -> list: # O(E + S^2)
     
     return G
 
-def size_of_graph(E:list) -> int: # O(E + S^2)
+def new_vertex(E:list, S:list) -> None: # O(S), czyli O(V) w najgorszym przypadku, dodajemy 1 wierzchołek i max. V krawędzi
+    V = size_of_graph(E)
+    for vertex in S:
+        E.append((V, vertex, 0))
+
+def size_of_graph(E:list) -> int: # O(E + S), czyli O(E + V)
     result = -1
     for edge in E:
         result = max(result, edge[0], edge[1])
@@ -50,8 +54,3 @@ def size_of_graph(E:list) -> int: # O(E + S^2)
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
 runtests( spacetravel, all_tests = True )
-"""E=[(0, 1, 5), (1, 2, 21), (1, 3, 1), (2, 4, 7), (3, 4, 13), (3, 5, 16), (4, 6, 4), (5, 6, 1)]
-S=[0, 2, 3]
-a=1
-b=5
-print(spacetravel(8, E, S, a, b))"""
