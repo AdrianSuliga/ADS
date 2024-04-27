ff4ae9bb9b3e8dfd1aa2de5a33989da2549f9c8d
Adrian Suliga <adriansuliga81@gmail.com>
2024-04-27 17:41:47 +0200

    find hamiltionian path in dag

--- Lab6/sciezka_hamiltona_dag.py
+++ Lab6/sciezka_hamiltona_dag.py
@@ -21,11 +21,43 @@ def hamiltonian_path(G):
             return False
     return True
 
+def topologic_sort(G:list) -> bool:
+    n = len(G)
+    visited = [False for _ in range(n)]
+    result = []
+
+    for u in range(n):
+        if not visited[u]:
+            DFSVisit(G, u, visited, result)
+            result.append(u)
+    
+    reverse(result)
+    
+    for i in range(n - 1):
+        if not result[i + 1] in G[result[i]]:
+            return False
+    return True
+
+def DFSVisit(G:list, u:int, visited:list, result:list) -> None:
+    visited[u] = True
+    for v in G[u]:
+        if not visited[v]:
+            DFSVisit(G, v, visited, result)
+            result.append(v)
+
+def reverse(T:list) -> None:
+    i, j = 0, len(T) - 1
+    while i < j:
+        T[i], T[j] = T[j], T[i]
+        i += 1
+        j -= 1
+
 G = [
     [1],
-    [2, 4],
     [3],
-    [4],
-    []
+    [1, 3, 0],
+    [4, 5],
+    [],
+    [4]
 ]
-print(hamiltonian_path(G))
+print(topologic_sort(G))
