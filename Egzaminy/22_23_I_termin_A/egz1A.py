f884908b5fa9df45e4a52e57c1a1ce417b95f4d2
Adrian Suliga <adriansuliga81@gmail.com>
2024-05-07 20:55:07 +0200

    graph exam 22_23 brute force

--- Egzaminy/22_23_I_termin_A/egz1A.py
+++ Egzaminy/22_23_I_termin_A/egz1A.py
@@ -1,7 +1,12 @@
 from queue import PriorityQueue
 from egz1Atesty import runtests
 
-def gold(G,V,s,t,r):
+# Adrian Suliga
+# Algorytm oblicza koszt drogi złycerza w każdym z 2V przypadków:
+# obrabował 1. zamek, nie obrabował 1. zamku, obrabował 2. zamek, nie obrabował 2. zamku, itd.
+# gdy rozważamy obrabowanie i. zamku, to algorytm 
+
+def gold(G, V, s, t, r):
     n = len(G)
     result = float('inf')
     for u in range(n):
@@ -9,14 +14,14 @@ def gold(G,V,s,t,r):
     result = min(result, standard_dijkstra(G, s, t))
     return result
 
-def standard_dijkstra(G, s, t):
+def standard_dijkstra(G, s, t) -> int:
     n = len(G)
     d = [float('inf') for _ in range(n)]
     Q = PriorityQueue()
 
-    d[s] = 0
     for u in range(n):
         Q.put((d[u], u))
+    d[s] = 0
 
     while not Q.empty():
         _, u = Q.get()
@@ -27,17 +32,44 @@ def standard_dijkstra(G, s, t):
 
     return d[t]
 
-def cheapest_path(G:list, gold, u, s, t, r):
-    n = len(G)
+def cheapest_path(P:list, gold, x, s, t, r) -> int:
+    n = len(P)
+    G = [[] for _ in range(2 * n)]
+    for u in range(n):
+        for v in P[u]:
+            G[u].append(v)
     for i in range(n):
-        G.append([])
-    for i in range(n):
-        for j in G[i]:
-            G[i + n].append((G[i][j][0] + n, 2 * G[i][j][1] + r))
-    G[u].append((u + n, 0))
-    G[u + n].append((u, 0))
-    return gold - min(standard_dijkstra(G, s, t), standard_dijkstra(G, s, t + n))
+        for j, cost in G[i]:
+            G[i + n].append((j + n, 2 * cost + r))
 
+    G[x].append((x + n, 0))
+    G[x + n].append((x, 0))
+
+    Q = PriorityQueue()
+    d = [float('inf') for _ in range(2 * n)]
+    for u in range(n):
+        Q.put((d[u], u))
+    d[s] = 0
+
+    while not Q.empty():
+        _, u = Q.get()
+        for v, cost in G[u]:
+            if d[u] + cost < d[v]:
+                d[v] = d[u] + cost
+                Q.put((d[v], v))
+ 
+    return min(d[t], d[t + n] - gold)
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( gold, all_tests = False )
+runtests( gold, all_tests = True )
+"""G = [
+    [(1,9), (2,2)],
+    [(0,9), (3,2), (4,6)],
+    [(0,2), (3,7), (5,1)],
+    [(1,2), (2,7), (4,2), (5,3)],
+    [(1,6), (3,2), (6,1)],
+    [(2,1), (3,3), (6,8)], 
+    [(4,1), (5,8)]
+]
+V = [25,30,20,15,5,10,0]
+print(cheapest_path(G, 30, 1, 0, 6, 7))"""
