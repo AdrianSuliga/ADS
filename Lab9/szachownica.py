a307961fa26e2c0839e4a68c46eecc38c0e57ed1
Adrian Suliga <adriansuliga81@gmail.com>
2024-06-01 14:24:47 +0200

    chessboard exs

--- Lab9/szachownica.py
+++ Lab9/szachownica.py
@@ -5,3 +5,36 @@
 
 # f(i, j) = min { f(i, j - 1), f(i - 1, j) } + A[i][j]
 # f(0, 0) = A[0][0]
+
+def tab_king(M:list) -> int: # O(n^2) czas i pamięć
+    n = len(M)
+    F = [[float('inf') for _ in range(n)] for _ in range(n)]
+
+    F[0][0] = M[0][0]
+
+    for i in range(n):
+        for j in range(n):
+            if i == j == 0: continue
+            if i > 0: F[i][j] = min(F[i][j], F[i - 1][j])
+            if j > 0: F[i][j] = min(F[i][j], F[i][j - 1])
+            F[i][j] += M[i][j]
+
+    return F[n - 1][n - 1]
+
+def dyn_king(M:list, i:int, j:int, memo:dict) -> int: # O(n^2) czas i pamięć
+    if (i, j) in memo: return memo[(i, j)]
+    if i == j == 0: return M[i][j]
+    if i < 0 or j < 0: return float('inf')
+    
+    result = min(dyn_king(M, i - 1, j, memo), dyn_king(M, i, j - 1, memo)) + M[i][j]
+    memo[(i, j)] = result
+    return result
+
+M = [
+    [1, 2],
+    [3, 1]
+]
+n = len(M) - 1
+
+print(dyn_king(M, n, n, {}))
+print(tab_king(M))
