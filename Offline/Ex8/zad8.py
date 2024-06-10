8f3ea657534d5f5cd939c1f22db90c7a1e31b945
Adrian Suliga <adriansuliga81@gmail.com>
2024-06-10 23:35:06 +0200

    offline 8

--- Offline/Ex8/zad8.py
+++ Offline/Ex8/zad8.py
@@ -1,16 +1,16 @@
-from egz2btesty import runtests
+from zad8testy import runtests
 from math import inf
 
 # Adrian Suliga
 # f(i, j) - optymalna suma odległości gdy rozważamy biurowce do i. włącznie oraz
 # i. biurowiec ma j. działkę
-# f(i, j) = abs(X[i] - Y[j]) + min { f(i - 1, k) | k < j }
+# f(i, j) = abs(X[i] - Y[j]) + min { f(i - 1, k) | i <= k < j }
 # Algorytm oblicza wartości funkcji f(i, j) opisanej powyżej i zapisuje je do tablicy F[][]
 # Na początku wypełniamy 0. wiersz wartościami |X[0] - Y[j]| gdzie 0 <= j < m
 # następnie tablicę F zapełniamy przechodząc wierszu po wierszu wedle powyższego wzoru rekurencyjnego
 # Złożoność czasową algorytmu oceniam na O(m^2 * n), a pamięciową na O(m * n)
 
-def parking(X, Y):
+def parking(X,Y):
     n, m = len(X), len(Y)
     F = [[inf for _ in range(m)] for _ in range(n)]
 
@@ -19,7 +19,7 @@ def parking(X, Y):
 
     for i in range(1, n): # zapełniamy tablicę wiersz po wierszu
         min_prev = F[i - 1][0] # sprytnie zapamiętując poprzednią minimalną wartość f(i - 1, j - 1) 
-        for j in range(i, m): # tak aby sprawnie policzyć min { f(i - 1, k) | k < j }
+        for j in range(i, m): # tak aby sprawnie policzyć min { f(i - 1, k) | i <= k < j }
             min_prev = min(min_prev, F[i - 1][j - 1])
             F[i][j] = abs(X[i] - Y[j]) + min_prev
 
