bac90d24cda8a68a8e1a06059e7a0fb86f9e283f
Adrian Suliga <adriansuliga81@gmail.com>
2024-06-13 00:06:50 +0200

    bit exs and solution for first one

---
+++ BIT/Dynamiki_Ćwiczenia/zad1k.py
@@ -0,0 +1,37 @@
+from zad1ktesty import runtests
+
+# Adrian Suliga
+# Algorytm oblicz wartości dla poniżej zdefiniowanej funkcji:
+# f(i, j) = różnica ilości zer i jedynek dla S[i : j + 1]
+# f(i, j) = f(i, j - 1) +- 1 zależnie czy S[j] == 0 czy S[j] == 1
+# f(i, i) = 1 dla S[i] == 0
+# f(i, i) = -1 dla S[i] == 1
+# f(i, j) = -1 gdy i > j
+
+def roznica( S ):
+    n, result = len(S), -1
+    F = [[-1 for _ in range(n)] for _ in range(n)] # Tablica na wartości funkcji f(i, j)
+
+    for i in range(n): # warunki początkowe dla każdego wiersza
+        if S[i] == '0': F[i][i] = 1
+        else: F[i][i] = -1
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            F[i][j] = F[i][j - 1]
+            if S[j] == '0': F[i][j] += 1
+            else: F[i][j] -= 1
+            result = max(result, F[i][j])
+
+    return result
+ 
+runtests ( roznica )
+#print(roznica("010001"))
+# 0 0 1
+
+# 1 0 0 0 0 0
+# 0-1 0 0 0 0
+# 0 0 1 0 0 0
+# 0 0 0 1 0 0
+# 0 0 0 0 1 0
+# 0 0 0 0 0-1
