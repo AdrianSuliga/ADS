c1b8e81ba62e1812298235a8770d9a9c5088a8e9
Adrian Suliga <adriansuliga81@gmail.com>
2024-06-25 23:14:32 +0200

    Bit exam 4

--- BIT/Egzaminy_Probne/egzP4a/egzP4a.py
+++ BIT/Egzaminy_Probne/egzP4a/egzP4a.py
@@ -1,7 +1,56 @@
 from egzP4atesty import runtests 
 
-def mosty ( T ):
-    #tutaj proszę wpisać własną implementację 
-    return 0
+# Adrian Suliga
+# Algorytm sortuje połączenia po pierwszej liczbie w krotce, następnie szuka
+# najdłuższego rosnącego podciągu wśród drugich liczb w krotkach w czasie O(n logn)
+# Szacuję złożoność czasową algorytmu na O(nlogn), a pamięciową na O(n)
 
-runtests ( mosty, all_tests=False )
+def mosty( T:list ) -> int:
+    n = len(T)
+    T.sort(key = lambda x : x[0])
+    A = [T[i][1] for i in range(n)]
+    return LIS(A)
+
+def LIS(A):
+    n = len(A)
+    R, cnt = [A[0]], 1
+
+    for i in range(1, n):
+        if A[i] > R[cnt - 1]: 
+            R.append(A[i])
+            cnt += 1
+        else: 
+            R[bin_search(R, A[i], 0, len(R) - 1)] = A[i]
+    
+    return len(R)        
+
+def bin_search(R, val, left, right):
+    if left > right: return left
+    pivot = (left + right) // 2
+
+    if R[pivot] < val:
+        return bin_search(R, val, pivot + 1, right)
+    else:
+        return bin_search(R, val, left, pivot - 1)
+
+# Adrian Suliga, podejście klasyczne, n^2
+# Algorytm sortuje tablicę po pierwszej liczbie każdej z krotek, następnie na tak posortowanej
+# tablicy wykonujemy algorytm szukania najdłuższego rosnącego podciągu wśród drugich liczb każdej
+# z krotek. Szacuję złożoność czasową algorytmu na O(n^2) a pamięciową na O(n).
+
+# f(i) - długość najdłuższego rosnącego podciągu kończącego się na T[i]
+# f(i) = max { f(j) + 1 | j < i i T[j] < T[i] }
+
+"""def mosty ( T:list ):
+    T.sort(key = lambda x : x[0])
+    n = len(T)
+    F = [1 for _ in range(n)]
+
+    for i in range(n):
+        for j in range(i):
+            if T[j][1] <= T[i][1]:
+                F[i] = max(F[i], F[j] + 1)
+
+    return max(F)"""
+
+runtests ( mosty, all_tests=True )
--- BIT/Egzaminy_Probne/egzP4b/egzP4b.py
+++ BIT/Egzaminy_Probne/egzP4b/egzP4b.py
@@ -1,16 +1,89 @@
 from egzP4btesty import runtests 
 
+# Adrian Suliga
+# Algorytm dla każdego wierzchołka tablicy T oblicza wartości jego poprzednika i następnika
+# następnie sprawdza czy wierzchołek jest piękny. Szacuję złożoność czasową algorytmu na
+# O(qh), a pamięciową na O(h) - wywołania rekurencyjne.
+
 class Node:
-  def __init__(self, key, parent):
-    self.left = None
-    self.right = None
-    self.parent = parent
-    self.key = key
-    self.x = None
+    def __init__(self, key, parent):
+        self.left = None
+        self.right = None
+        self.parent = parent
+        self.key = key
+        self.x = None
 
-
-def sol(root, T):
-
-    return 0
+def sol(root:Node, T:list[Node]) -> int:
+    result = 0
+    for node in T:
+        prev = node_prev(node) # obliczamy poprzednik
+        next = node_next(node) # obliczemy następnik
+        if prev is None or next is None: continue
+        if prev + next == 2 * node.key: result += node.key
     
+    return result
+
+def node_prev(node:Node):
+    if node.left is not None: # jeśli ma lewe poddrzewo, to
+        return max_val(node.left) # idziemy maksymalnie w dół na lewo
+    
+    parent_node = node.parent # w przeciwnym wypadku musimy szukać
+    while parent_node.left == node: # poprzednika wśród parentów
+        node = parent_node
+        parent_node = parent_node.parent
+    return parent_node.key
+
+def node_next(node:Node):
+    if node.right is not None:
+        return min_val(node.right)
+
+    parent_node = node.parent
+    while parent_node.right == node:
+        node = parent_node
+        parent_node = parent_node.parent
+    return parent_node.key
+
+def max_val(node:Node):
+    ptr = node
+    while ptr.right is not None:
+        ptr = ptr.right
+    return ptr.key
+
+def min_val(node:Node):
+    ptr = node
+    while ptr.left is not None:
+        ptr = ptr.left
+    return ptr.key
+
+# Adrian Suliga
+# Algorytm wyznacza z drzewa tablicę posortowanych wartości kluczy wierzchołków, następnie
+# dla każdego wierzchołka tablicy T wyszukujemy w posortowanej tablicy jego indeks, dzięki
+# czemu jesteśmy w stanie łatwo sprawdzić jego poprzednika i następnika. Szacuję złożoność
+# czasową algorytmu na O(n + qlogn), a pamięciową na O(n)
+"""def sol(root:Node, T:list[Node]):
+    A, result = [], 0
+    travel(root, A)
+    n = len(A)
+
+    for node in T:
+        x = bin_search(node.key, A, 0, n - 1)
+        if A[x - 1] + A[x + 1] == 2 * A[x]: result += A[x]
+
+    return result
+
+def travel(node:Node, T:list) -> None:
+    if node.left is not None:
+        travel(node.left, T)
+    T.append(node.key)
+    if node.right is not None:
+        travel(node.right, T)
+
+def bin_search(val, T, left, right) -> int:
+    if left > right: return -1
+    pivot = (left + right) // 2
+
+    if T[pivot] == val: return pivot
+    elif T[pivot] < val: return bin_search(val, T, pivot + 1, right)
+    else: return bin_search(val, T, left, pivot - 1)"""
+
 runtests(sol, all_tests = True)
